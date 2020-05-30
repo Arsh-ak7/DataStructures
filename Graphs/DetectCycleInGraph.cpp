4272d0ae22e6f1bb39dfbc8788ef6dc2bdf30d2a
Arsh-ak7 <arshkr07@gmail.com>
2020-05-30 12:06:14 +0530

    Create DetectCycleInGraph.cpp

---
+++ Graphs/DetectCycleInGraph.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+bool isCyclicUtil(vector<int> adj[],vector<bool> visited,int curr){
+if(visited[curr]==true)
+return true;
+visited[curr]=true;
+bool flag= false;
+for(int i=0;i<adj[curr].size();i++){
+    flag=isCyclicUtil(adj,visited,i);
+    if(flag==true)
+    return true;
+}
+return false;
+}
+
+
+bool isCyclic(vector<int>adj[],int v){
+    vector<bool> visited(v,false);
+    bool flag=false;
+    for(int i=0;i<v;i++){
+        visited[i]=true;
+        for(int j=0;j<adj[i].size();j++){
+            flag=isCyclicUtil(adj,visited,adj[i][j]);
+            if(flag==true)
+            return true;
+        }
+        visited[i]=false;
+    }
+    return false;
+}
+
+
+int main(){
+    int e,v;
+    cout<<"Enter NUmber of Vertices and Edges ";
+    cin>>v>>e;
+
+    vector<int>*adj=new vector<int>[v];
+    for(int i=0;i<e;i++){
+        cout<<"Enter src and dest ";
+        int src,dest;
+        cin>>src>>dest;
+        adj[src].push_back(dest);
+    }
+   cout<< isCyclic(adj,v);
+}
